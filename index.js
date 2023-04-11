e712b29e194ec0ff840d01893a8eb477ae05d9e2
chumak8088@gmail.com <k.smulska@pro-capital.local>
2023-04-11 23:02:10 +0300

    Math Oparations

---
+++ index.js
@@ -0,0 +1,20 @@
+const num1 = parseFloat (prompt (""));
+const num2 = parseFloat (prompt (""));
+const sum = num1 + num2;
+console.log(sum)
+alert(sum) 
+const num3 = parseFloat (prompt (""));
+const num4 = parseFloat (prompt (""));
+const dev = num3 - num4;
+console.log(dev)
+alert(dev) 
+const num5 = parseFloat (prompt (""));
+const num6 = parseFloat (prompt (""));
+const mult = num5 * num6;
+console.log(mult)
+alert(mult) 
+const num7 = parseFloat (prompt (""));
+const num8 = parseFloat (prompt (""));
+const div = num7 / num8;
+console.log(div)
+alert(div) 

b0d926943acf6a6b5910ecc026e718f9dfb1e773
chumak8088@gmail.com <k.smulska@pro-capital.local>
2023-04-12 12:23:54 +0300

    Math Operations

--- Math-Operation/index.js
+++ Math-Operation/index.js
@@ -2,19 +2,19 @@ const num1 = parseFloat (prompt (""));
 const num2 = parseFloat (prompt (""));
 const sum = num1 + num2;
 console.log(sum)
-alert(sum) 
+alert(num1 + "+" + num2 + "=" + sum) 
 const num3 = parseFloat (prompt (""));
 const num4 = parseFloat (prompt (""));
 const dev = num3 - num4;
 console.log(dev)
-alert(dev) 
+alert(num3 + "-" + num4 + "=" + dev) 
 const num5 = parseFloat (prompt (""));
 const num6 = parseFloat (prompt (""));
 const mult = num5 * num6;
 console.log(mult)
-alert(mult) 
+alert(num5 + "*" + num6 + "=" + mult) 
 const num7 = parseFloat (prompt (""));
 const num8 = parseFloat (prompt (""));
 const div = num7 / num8;
 console.log(div)
-alert(div) 
+alert(num7 + "/" + num8 + "=" + div) 

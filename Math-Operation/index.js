bd215a93bb8a75eb3b5325b0fea1be6aa30d9477
chumak8088@gmail.com <k.smulska@pro-capital.local>
2023-04-12 23:30:18 +0300

    Math Op

--- Math-Operation/index.js
+++ Math-Operation/index.js
@@ -1,20 +1,14 @@
-const num1 = parseFloat (prompt (""));
-const num2 = parseFloat (prompt (""));
-const sum = num1 + num2;
-console.log(sum)
-alert(num1 + "+" + num2 + "=" + sum) 
-const num3 = parseFloat (prompt (""));
-const num4 = parseFloat (prompt (""));
-const dev = num3 - num4;
-console.log(dev)
-alert(num3 + "-" + num4 + "=" + dev) 
-const num5 = parseFloat (prompt (""));
-const num6 = parseFloat (prompt (""));
-const mult = num5 * num6;
-console.log(mult)
-alert(num5 + "*" + num6 + "=" + mult) 
-const num7 = parseFloat (prompt (""));
-const num8 = parseFloat (prompt (""));
-const div = num7 / num8;
-console.log(div)
-alert(num7 + "/" + num8 + "=" + div) 
+let num1 = prompt ("");
+let num2 = prompt ("");
+num1 = Number(num1);
+num2 = Number(num2);
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+alert(`
+    ${num1}+${num2}=${sum}
+    ${num1}-${num2}=${sub}
+    ${num1}*${num2}=${mult}
+    ${num1}/${num2}=${div}
+`)
